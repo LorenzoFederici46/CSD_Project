65fda15f20465051f62d4dc94ab3d9be4ccd471f
LorenzoFederici46 <95763830+LorenzoFederici46@users.noreply.github.com>
2022-12-05 16:50:33 +0100

    Creato file per la connessione tra foglio excel e database.

---
+++ myApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using IronXL;
+using System;
+namespace myApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var workbook = WorkBook.Load(@"C:\Users\lenovo\Desktop\Personal File\Unicam\4°anno\Complex System Design\Database\Clusterizzazione UNICAM.xlsx");
+            var worksheet = workbook.GetWorkSheet("Sheet1");
+            var cells = worksheet[$"A{2}:I{2}"].ToList();
+
+            foreach(var itr in cells)
+            {
+                Console.WriteLine(itr.Text);
+
+            }
+
+        }
+    }
+}
